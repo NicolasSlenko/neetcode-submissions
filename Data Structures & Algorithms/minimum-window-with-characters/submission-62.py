41a0d52e93b26917ddb2e404733705effa86475a
Nicolas Slenko <101363647+NicolasSlenko@users.noreply.github.com>
2026-07-04 14:37:50 -0700

    Add: minimum-window-with-characters - submission-62

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-62.py
@@ -0,0 +1,62 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s) or len(t) == 0 or len(s) == 0:
+            return ""
+
+        res = ""
+        max_length = float('inf')
+
+        if s == t:
+            return s
+        
+        window = defaultdict(int)
+        need = defaultdict(int)
+        have_c = 0
+        need_c = 0
+
+        for char in t:
+            need[char] += 1
+        
+        for key in need:
+            need_c +=1 
+        
+        l = r = 0
+        
+        while r < len(s):
+            window[s[r]] += 1
+            if window[s[r]] == need[s[r]]:
+                have_c += 1
+            
+            while have_c >= need_c:
+                if r - l + 1 < max_length:
+                    res = s[l:r+1]
+                    max_length = r-l+1
+                window[s[l]] = window.get(s[l], 0) - 1
+                if window[s[l]] < need[s[l]]:
+                    have_c -= 1
+                l += 1
+            r += 1
+
+        r-=1
+        window[s[r]] += 1
+
+        while l < r:
+            if window[s[r]] == need[s[r]]:
+                have_c += 1
+            while have_c >= need_c:
+                if r - l + 1 < max_length:
+                    res = s[l:r+1]
+                    max_length = r-l+1
+                window[s[l]] = window.get(s[l], 0) - 1
+                if window[s[l]] < need[s[l]]:
+                    have_c -= 1
+            l += 1
+            
+        print(r)
+        print(max_length)
+        print(have_c)
+        print(need_c)        
+        return res
+        
+
+        
